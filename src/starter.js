bc423419def866027aac2e85faf8b659664e0695
kyoungminSong <162668642+kyoungminSong@users.noreply.github.com>
2024-04-19 14:28:12 +0900

    modified

--- src/starter.js
+++ src/starter.js
@@ -7,8 +7,8 @@ import "./viz.css";
 // svg
 const svg = d3.select("#svg-container").append("svg").attr("id", "svg");
 
-const width = parseInt(d3.select("#svg-container").style("width"));
-const height = parseInt(d3.select("#svg-container").style("height"));
+let width = parseInt(d3.select("#svg-container").style("width"));
+let height = parseInt(d3.select("#svg-container").style("height"));
 const margin = { top: 65, right: 50, bottom: 65, left: 50 };
 // console.log(width + "," + height);
 
@@ -53,15 +53,38 @@ const radarLine = d3
 // data
 
 let data = [];
-let selectedPlayer;
+let selectedPlayer; //레이더차트 만드는 데이터
 
 let radiusAxis, angleAxis, labels;
-let Path;
+let path;
+
+let players;
+let selectedName = "H. Son";
 
 d3.json("data/fifa23_maleplayers.json").then((raw_data) => {
   data = raw_data.filter((d) => d.overall > 85); //데이터 필터링
-  selectedPlayer = data.filter((d) => d.short_name == "H. Son")[0]; //원하는 선수 필터링
+  selectedPlayer = data.filter((d) => d.short_name == selectedName)[0]; //원하는 선수 필터링
   // console.log(selectedPlayer);
+
+  players = [...new Set(data.map((d) => d.short_name))];
+  // console.log(players);
+
+  const dropdown = document.getElementById("options");
+
+  players.map((d) => {
+    const option = document.createElement("option");
+    option.value = d; //value=컴퓨터가 인식하는 데이터
+    option.innerHTML = d; //innerHTML=페이지에 나타나는 값
+    option.selected = d === selectedName ? true : false; //조건?A:B -> true이면 A, false이면 B (dropdown에서 무엇이 선택되는지)
+    //option.selected = d === 'Neymar Jr' ? true : false;
+    dropdown.appendChild(option);
+  });
+
+  dropdown.addEventListener("change", function () {
+    selectedName = dropdown.value;
+    // console.log(selectedName);
+    updatePlayer(); //updatePlayer 함수실행
+  });
 
   // axis
   radiusAxis = g
@@ -101,7 +124,7 @@ d3.json("data/fifa23_maleplayers.json").then((raw_data) => {
     .text((d) => d)
     .attr("class", "labels")
     .attr("fill", "white")
-    .attr("fill-opacity", 0.5);
+    .attr("fill-opacity", 0.3);
 
   path = g
     .append("path")
@@ -109,8 +132,10 @@ d3.json("data/fifa23_maleplayers.json").then((raw_data) => {
     .attr("d", radarLine)
     .attr("fill", pointColor)
     .attr("stroke", pointColor)
-    .attr("stroke-width", 1.5)
+    .attr("stroke-width", 1.3)
     .style("fill-opacity", 0.25);
+
+  d3.select("#player-name").text(selectedPlayer.long_name);
 });
 
 ////// function
@@ -123,3 +148,44 @@ const getYPos = (dist, index) => {
   // radius * sin (theta)
   return radiusScale(dist) * Math.sin(angleScale(index) - Math.PI / 2);
 };
+
+////////////////////////////////////////////////////////////////////
+/////////////////////////////  Update  /////////////////////////////
+
+const updatePlayer = () => {
+  selectedPlayer = data.filter((d) => d.short_name == selectedName)[0];
+  console.log(selectedPlayer);
+
+  radarLine.radius((d) => radiusScale(selectedPlayer[d]));
+
+  path.transition().duration(500).attr("d", radarLine); //duration(1000)이 1초
+
+  d3.select("#player-name").text(selectedPlayer.long_name);
+};
+
+// Resize
+window.addEventListener("resize", () => {
+  width = parseInt(d3.select("#svg-container").style("width"));
+  height = parseInt(d3.select("#svg-container").style("height"));
+  // console.log(width + ", " + height);
+  g.attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  //scale
+  minLen = d3.min([height / 2 - margin.top, width / 2 - margin.right]);
+  radiusScale.range([0, minLen]);
+
+  //axis
+  radiusAxis.attr("r", (d) => radiusScale(d));
+
+  angleAxis
+    .attr("x2", (d, i) => getXPos(100, i))
+    .attr("y2", (d, i) => getYPos(100, i));
+
+  radarLine.radius((d) => radiusScale(selectedPlayer[d]));
+
+  path.attr("d", radarLine);
+
+  labels
+    .attr("x", (d, i) => getXPos(120, i)) //숫자는 100보다만 크면 됨
+    .attr("y", (d, i) => getYPos(120, i));
+});
